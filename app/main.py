daaefe2018ef71dd7cdc18bac32b3ec6523081eb
Yaroslav Labaz <kpxmedet@gmail.com>
2025-06-27 18:55:45 +0300

    code review fixes

--- app/main.py
+++ app/main.py
@@ -35,7 +35,7 @@ class FlyingRobot(BaseRobot):
         self,
         name: str,
         weight: int,
-        coords: list = [0, 0, 0]
+        coords: tuple | list = (0, 0, 0)
     ) -> None:
         super().__init__(name=name, weight=weight, coords=coords)
 
@@ -53,7 +53,7 @@ class DeliveryDrone(FlyingRobot):
         weight: int,
         max_load_weight: int,
         current_load: Cargo | None = None,
-        coords: list = [0, 0, 0]
+        coords: tuple | list = (0, 0, 0)
     ) -> None:
         super().__init__(name=name, weight=weight, coords=coords)
         self.max_load_weight = max_load_weight
